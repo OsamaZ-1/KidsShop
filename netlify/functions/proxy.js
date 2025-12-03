b6fd3b4b26e8954c1073ff0d16d0b6be1f8d0b8d
SpellGear Software <spellgearsoftware@gmail.com>
2025-12-03 04:56:48 -0800

    fixed proxy

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,5 +1,3 @@
-import fetch from "node-fetch";
-
 export async function handler(event, context) {
   const url = `https://script.google.com/macros/s/AKfycbwXG6E-pYy70J5niCgD9O7vATeLtktX_B8ODDAVLoE_jzmdpAbf-HV3wdSYuBzOdlqoTQ/exec`;
 
@@ -8,11 +6,11 @@ export async function handler(event, context) {
   const fullUrl = query ? `${url}?${query}` : url;
 
   const response = await fetch(fullUrl);
-  const data = await response.text(); // or .json() depending on response
+  const data = await response.json(); // or .json() depending on response
 
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ data }),
+    body: JSON.stringify(data),
   };
 }
